61e9adccaa94e8a8f4444b92ed04c8f5bdc7e81a
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-05-31 19:39:27 -0300

    Exercise 09 solved!

--- Cpp_solving/exercise_09.cpp
+++ Cpp_solving/exercise_09.cpp
@@ -4,7 +4,51 @@
 #include <iostream>
 using namespace std;
 
+// I created a function to request an integer data
+void request_int_data(string message, int &value){
+    cout << message;
+    cin >> value;
+}
+
+int how_many_days(){
+    int year = 0, month = 0, days = 0;
+    request_int_data("Insert a year: ", year);
+    request_int_data("Insert a month: ", month);
+
+    switch (month)
+    {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        days = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        days = 30;
+        break;
+    case 2:
+        if ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0))
+        {
+            days = 29;
+        }
+        else days = 28;
+        break;
+    default:
+        cout << "You didn't insert a value, try again.";
+        break;
+    }
+    return days;
+}
+
 int main () {
- 
+    int days = how_many_days();
+
+    cout << "The month that you inserted has " << days << " days!";
  return 0;
 }
