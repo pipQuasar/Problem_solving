2ecdac5cffc2ad1ae8b2353a9adcdd5a7917de76
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-01 20:26:08 -0300

    Creating 10 solved!

---
+++ Python_solving/exercise_10.py
@@ -0,0 +1,5 @@
+# Enter an age, display one of the following legends on the screen:
+#     ❖ 'minor' if the age is less than or equal to 12
+#     ❖ 'cadet' if the age is between 13 and 18
+#     ❖ 'juvenile' if the age is greater than 18 and does not exceed 26
+#     ❖ 'major' in the case that does not meet any of the previous conditions
