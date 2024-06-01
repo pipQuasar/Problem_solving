ea5589e0c44e98fab4e751d33b57d3a7bc27b445
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-05-31 22:53:20 -0300

    Exercise 07 solved!

--- Python_solving/exercise_07.py
+++ Python_solving/exercise_07.py
@@ -4,10 +4,10 @@
 
 # I create the resolutive function
 def form_or_not_a_triangle(side1,side2,side3):
-    if ((side1 == side2 and side1 != side3) or 
-        (side2 == side3 and side2 != side1) or 
-        (side3 == side1 and side3 != side2)):
+    if ((side1 == side2 != side3) or 
+        (side2 == side3 != side1) or 
+        (side3 == side1 != side2)):
         print("They form a triangle!")
     else: print("They do not form a triangle")
 # Show it
-form_or_not_a_triangle(1,2,1)
+form_or_not_a_triangle(1,3,3)
