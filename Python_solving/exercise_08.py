c483890160886b6313b57a502a806067f0d19210
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-01 20:08:45 -0300

    Exercise 08 solved!

--- Python_solving/exercise_08.py
+++ Python_solving/exercise_08.py
@@ -1,2 +1,11 @@
 # Given a triangle represented by its sides L1, L2, L3, determine and print a legend as appropriate:
 #  equilateral, isosceles or scalenes.
+def wich_triangle(side1,side2,side3):
+    if ((side1 == side2 != side3) or 
+        (side2 == side3 != side1) or 
+        (side3 == side1 != side2)):
+        print("They form a isosceles!")
+    elif (side1 == side2 == side3):print("They form a equilateral")
+    else: print("They form a scalenes")
+    
+wich_triangle(1,3,2)
