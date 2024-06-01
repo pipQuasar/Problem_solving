e4dfc9e33ac19ff20f9e8435c2bbefa99f78b519
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-05-31 22:50:51 -0300

    Exercise 07 solved!

--- Python_solving/exercise_07.py
+++ Python_solving/exercise_07.py
@@ -1,2 +1,13 @@
 # Given three values, determine and print a legend accordingly: 
 #  “They form a triangle” or “They do not form a triangle.”
+# In this exercise I am going to use an isosceles triangle.
+
+# I create the resolutive function
+def form_or_not_a_triangle(side1,side2,side3):
+    if ((side1 == side2 and side1 != side3) or 
+        (side2 == side3 and side2 != side1) or 
+        (side3 == side1 and side3 != side2)):
+        print("They form a triangle!")
+    else: print("They do not form a triangle")
+# Show it
+form_or_not_a_triangle(1,2,1)
