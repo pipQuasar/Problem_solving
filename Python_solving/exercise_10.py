5b76ddfa94cac2572604b61378939ec446b1a70c
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-01 20:59:33 -0300

    Exercise 10 solved!

--- Python_solving/exercise_10.py
+++ Python_solving/exercise_10.py
@@ -3,3 +3,11 @@
 #     ❖ 'cadet' if the age is between 13 and 18
 #     ❖ 'juvenile' if the age is greater than 18 and does not exceed 26
 #     ❖ 'major' in the case that does not meet any of the previous conditions
+
+def what_leyend(age):
+    if (age <= 12): print("You're 'minor'")
+    elif (age <= 18): print("You're 'cadet'")
+    elif (age <= 26): print("You're 'juvenile'")
+    else: print("You're 'major'")
+    
+what_leyend(11)
