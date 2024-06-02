e320d802e04dec5f6829314f8a1f52765f0d5b15
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-01 21:26:33 -0300

    Exercise 11 solved!

--- Cpp_solving/exercise_11.cpp
+++ Cpp_solving/exercise_11.cpp
@@ -1,3 +1,18 @@
 /*
     Report the first 100 natural numbers and their sum
 */
+
+#include <iostream>
+using namespace std;
+
+int main () {
+ int sum = 0;
+ for (int i = 1; i <= 100; i++)
+ {
+    cout << i << endl;
+    sum += i;
+ }
+
+ cout << "Final sum result: " << sum;
+ return 0;
+}
