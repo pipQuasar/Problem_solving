cb5d182b08b9af482d399373782650b5d1dc086b
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-02 01:45:20 -0300

    Exercise 12 solved!

--- Cpp_solving/exercise_12.cpp
+++ Cpp_solving/exercise_12.cpp
@@ -4,7 +4,16 @@
 #include <iostream>
 using namespace std;
 
+int sucesive_sums(int N, int M){
+    int result = 1;
+    for (int i = 1; i <= M; i++)
+    {
+        result = N*i;
+    }
+    return result;
+}
+
 int main () {
- 
+    cout << "result: " << sucesive_sums(3,15);
  return 0;
 }
