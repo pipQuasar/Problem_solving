f6edfd257e7a3fc0dff55ce54958e9b68b7a4be9
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-02 01:47:19 -0300

    Creating exercise 13!

---
+++ Cpp_solving/exercise_13.cpp
@@ -0,0 +1,11 @@
+/*
+    Given 50 integers, report the average of those greater than 100 and the sum of those less than
+    –10.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
