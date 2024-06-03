c303aa2c22a73518e8c9986d1ada88f8437997fc
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-03 20:06:08 -0300

    Exercise 13 solved!

--- Cpp_solving/exercise_13.cpp
+++ Cpp_solving/exercise_13.cpp
@@ -1,13 +1,40 @@
 /*
-    Given 50 integers, report the average of those greater than 100 and the sum of those less than
+    Given 50 integers, report A) the average of those greater than 100 and B) the sum of those less than
     –10.
 */
 #include <iostream>
 using namespace std;
 
+void request_int_data(string message, int &value){
+    cout << message;
+    cin >> value;
+}
 
+void A_and_B(){
+    int value = 0;
+    int averager = 0, average = 0, sum_graters_than = 0;
+    int sum_less_than = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        request_int_data("Enter a value: ", value);
+        if (value > 100)
+        {
+            sum_graters_than += value;
+            averager++;
+        }
+        if (value < -10)
+        {
+            sum_less_than+=value;
+        }
+    }
+    average = sum_graters_than / averager;
+
+    cout << "The average of those greater than 100 is: " << average << endl;
+    cout << "The sum of those less than -10 is: " << sum_less_than;
+}
 
 int main () {
- 
+    A_and_B();
  return 0;
 }
