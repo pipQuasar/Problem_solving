9ccacc1847319761bb9b79c425bd383c08e24b4a
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-03 20:07:44 -0300

    Creating exercise 14!

---
+++ Cpp_solving/exercise_14.cpp
@@ -0,0 +1,18 @@
+/*
+    An inspection is carried out at a paint factory, and 20 violations were detected. For each violation
+    took note of the following data:
+    - Type of Violation (1, 2, 3, or 4)
+    - Reason for the violation
+    - Value of the fine
+    - Seriousness of the infraction ('L', 'M', 'G')
+    It is requested to report at the end of the process:
+    • The total values ​​of the fine to pay according to the type of severity.
+    • The legend “Close factory” if the number of violations 3 and 4 with severity “G” are greater than 3.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
