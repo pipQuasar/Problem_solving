7ebb477340169ca61f0abe9c5cfbe8fb974f5e64
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-04 20:32:17 -0300

    Creating exercise 17!

---
+++ Cpp_solving/exercise_17.cpp
@@ -0,0 +1,11 @@
+/*
+    Given a value M, determine and issue a list with the first M multiples of 3 that are not multiples of 5,
+    within the set of natural numbers.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
