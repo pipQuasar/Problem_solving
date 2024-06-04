c6e51bf546ef2d93cd1977d1d11c2b82189bb49a
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-04 17:08:24 -0300

    Creating exercise 15!

---
+++ Cpp_solving/exercise_15.cpp
@@ -0,0 +1,11 @@
+/*
+    Enter and report values, as long as the value entered is not negative. Report the number of values
+    entered.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
