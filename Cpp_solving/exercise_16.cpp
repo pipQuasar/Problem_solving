c8f91e9344c9c3572b164921d2850eea8f354446
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-04 17:30:24 -0300

    Creating exercise 16!

---
+++ Cpp_solving/exercise_16.cpp
@@ -0,0 +1,17 @@
+/*
+    A set of float values ​​is entered, each representing the salary of an employee,
+    except the last value which is zero and indicates the end of the set. It is requested to 
+    develop a program that determines and reports:
+        a) How many employees earn less than $1,520.
+        b) How many earn $1,520 or more but less than $2,780.
+        c) How many earn $2,780 or more but less than $5,999.
+        d) How many earn $5,999 or more.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
