7fc644cf6d3cd55bb7d3077a097cce80ed56f2eb
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-04 17:29:00 -0300

    Exercise 15 solved!

--- Cpp_solving/exercise_15.cpp
+++ Cpp_solving/exercise_15.cpp
@@ -5,7 +5,23 @@
 #include <iostream>
 using namespace std;
 
+void request_int_data(string message, int &value){
+    cout << message;
+    cin >> value;
+}
+
+int number_of_values(){
+    int value = 0, number_of_values = 0;
+    while (value >= 0)
+    {
+        request_int_data("Enter a value: ", value);
+        cout << "Entered value: " << value << endl << endl;
+        number_of_values++;
+    }
+    return number_of_values;
+}
+
 int main () {
- 
+    cout << "Number of values: " << number_of_values();
  return 0;
 }
