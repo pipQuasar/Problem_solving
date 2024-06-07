2586787f78804fed775e4f91692783afdebfcbde
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-07 20:36:03 -0300

    Creating exercise 21!

---
+++ Cpp_solving/exercise_21.cpp
@@ -0,0 +1,14 @@
+/*
+    Given a set of values, ending with a null value, determine and print (if there were values):
+    a) The maximum negative value
+    b) The minimum positive value
+    c) The minimum value within the range -17.3 and 26.9
+    d) The average of all values.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
