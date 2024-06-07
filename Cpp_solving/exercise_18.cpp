a570b2e491a6850d85978141c8c6b66303bb9ccc
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-07 17:13:53 -0300

    Exercise 18 solved!

--- Cpp_solving/exercise_18.cpp
+++ Cpp_solving/exercise_18.cpp
@@ -4,7 +4,22 @@
 #include <iostream>
 using namespace std;
 
+void request_int_data(string message, int &value){
+    cout << message;
+    cin >> value;
+}
+
+void largest_of_ten(){
+    int value = 0, largest = INT32_MIN;
+    for (int i = 0; i < 10; i++)
+    {
+        request_int_data("Enter a value: ", value);
+        if (value > largest) largest = value; 
+    }
+    cout << "The largest number is: " << largest;
+}
+
 int main () {
- 
+    largest_of_ten();
  return 0;
 }
