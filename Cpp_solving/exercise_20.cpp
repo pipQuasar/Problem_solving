61fe078c0abedc2b557d2e552d9bfb73eb343067
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-07 17:37:36 -0300

    Creating exercise 20!

---
+++ Cpp_solving/exercise_20.cpp
@@ -0,0 +1,11 @@
+/*
+    Given a set of Names and Birthdates (YYYYMMDD), ending with a Name =
+    'END', inform the name of the oldest person and the youngest.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
