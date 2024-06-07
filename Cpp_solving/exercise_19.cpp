31d5fe210f0d32dc33496e22b2048723ebbcebc7
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-07 17:15:15 -0300

    Creatin exercise 19!

---
+++ Cpp_solving/exercise_19.cpp
@@ -0,0 +1,11 @@
+/*
+    Given N values, report the largest, smallest, and at what position 
+    in the set they were entered.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
