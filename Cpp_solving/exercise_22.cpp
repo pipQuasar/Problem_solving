d209420ec163dc9e6b25e8a46618e1b18c683e94
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-09 17:54:17 -0300

    Creating exercise 22!

---
+++ Cpp_solving/exercise_22.cpp
@@ -0,0 +1,19 @@
+/*
+    A cargo ship transports 100 containers to three different ports in the country.
+    The ports are identified with numbers 1, 2 and 3.
+    The following data is recorded for each container that the ship transports:
+    - Container identification
+    - Container weight in kg
+    - Port of arrival (a value from 1 to 3).
+    It is requested to calculate and report:
+    1) The total weight that the ship must carry
+    2) Identification of the heaviest container
+    3) The number of containers that must be transported to each port
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
