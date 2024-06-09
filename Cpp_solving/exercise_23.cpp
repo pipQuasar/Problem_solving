1c2ad170f580e05949847a1ceaa89eecc2f20291
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-06-09 20:21:29 -0300

    Creating exercise 23!

---
+++ Cpp_solving/exercise_23.cpp
@@ -0,0 +1,16 @@
+/*
+    K teams participate in a soccer tournament. The tournament is played with the round-robin system. By
+    For each match played by a team, the following information is available:
+    a) Equipment number,
+    b) Result code ('P'= Lost, 'E'= Tied, 'G'= Won).
+    A batch of data is assembled with all the results of the tournament, grouped by team number.
+    Develop the program that prints:
+    For each team, its number and the total score it obtained (add 3 if it wins, and 1 if it ties).
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+ return 0;
+}
